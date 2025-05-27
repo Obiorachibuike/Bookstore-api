0564eb81b4bdb1593dc4dd36e96fe4510ab226a8
Chibuike Obiora <104082031+Obiorachibuike@users.noreply.github.com>
2025-05-27 17:50:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bookRoutes = require('./routes/bookRoutes');
 
 const app = express();
 app.use(express.json());
-app.use('/api', bookRoutes);
+app.use('/api', bookRoute);
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
