db9167a12d319b22178ab49d1958d0462e73449b
Chibuike Obiora <104082031+Obiorachibuike@users.noreply.github.com>
2025-05-28 07:15:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', bookRoutes);
 /*
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://User:Fanthom456world@cluster0.paigpn8.mongodb.net/bookstore_api?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
   .catch(err => console.log(err));
 
